296cfa6f96b878464d4ada3f68f2a34ce4b09a8d
Jason Heppler <jason.heppler@gmail.com>
2024-01-18 22:13:02 -0600

    Adding neorg for org support

--- init.lua
+++ init.lua
@@ -43,6 +43,31 @@ return {
     	end
   	},
 
+  	-- neorg 
+  	{
+    	"nvim-neorg/neorg",
+    	build = ":Neorg sync-parsers",
+    	lazy = false,
+    	auto = true,
+    	-- tag = "*",
+    	dependencies = { "nvim-lua/plenary.nvim" },
+    	config = function()
+      	require("neorg").setup {
+        	load = {
+          	["core.defaults"] = {}, -- Loads default behaviour
+          	["core.concealer"] = {}, -- Adds pretty icons to your documents
+          	["core.dirman"] = { -- Manages Neorg workspaces
+            	config = {
+              	workspaces = {
+                	notes = "~/notes",
+              	},
+            	},
+          	},
+        	},
+      	}
+    	end,
+  	},
+
 		-- Trouble
 		{
 			"folke/trouble.nvim",
